9d81e8a94cf156f6727893f140c7047f1c31fbbe
Francisco-Mendes-96529 <80208351+Francisco-Mendes-96529@users.noreply.github.com>
2022-11-05 19:00:15 +0000

    project2 comment

--- project2/final/mote_sensors/mote_sensors.c
+++ project2/final/mote_sensors/mote_sensors.c
@@ -64,87 +64,86 @@
 #define SERVICE_ID 190
 
 #define STR(x) #x
-#define LIGHTS_TIME (300 * CLOCK_SECOND)
-#define TEMPERATURE_TIME ((30 + (float)rand()/RAND_MAX * 5) * CLOCK_SECOND)
+#define LIGHTS_TIME(300 * CLOCK_SECOND)
+#define TEMPERATURE_TIME((30 + (float) rand() / RAND_MAX * 5) * CLOCK_SECOND)
 #define TEMPERATURE_MAX 35
 
 static struct simple_udp_connection unicast_connection;
 static int temp_k = 0;
 static float temp_debug = 0;
-static float temperature[] = {0,0,0,0,0,0};
+static float temperature[] = {0,0,0,0,0};
 
 /*---------------------------------------------------------------------------*/
 PROCESS(button_process, "Button process");
 PROCESS(temperature_process, "Temperature process");
 PROCESS(temperature_sampling_process, "Temperature sampling process");
 PROCESS(unicast_sender_process, "Unicast sender example process");
-AUTOSTART_PROCESSES(&button_process, &temperature_process, &temperature_sampling_process, &unicast_sender_process);
+AUTOSTART_PROCESSES( & button_process, & temperature_process, & temperature_sampling_process, & unicast_sender_process);
 /*---------------------------------------------------------------------------*/
 static void
-receiver(struct simple_udp_connection *c,
-         const uip_ipaddr_t *sender_addr,
-         uint16_t sender_port,
-         const uip_ipaddr_t *receiver_addr,
-         uint16_t receiver_port,
-         const uint8_t *data,
-         uint16_t datalen)
-{
+receiver(struct simple_udp_connection * c,
+        const uip_ipaddr_t * sender_addr,
+        uint16_t sender_port,
+        const uip_ipaddr_t * receiver_addr,
+        uint16_t receiver_port,
+        const uint8_t * data,
+        uint16_t datalen) {
   printf("Data received on port %d from port %d with length %d\n",
-         receiver_port, sender_port, datalen);
+    receiver_port, sender_port, datalen);
 }
 /*---------------------------------------------------------------------------*/
 static void
-set_global_address(void)
-{
+set_global_address(void) {
   uip_ipaddr_t ipaddr;
   int i;
   uint8_t state;
 
-  uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
-  uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
-  uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
+  uip_ip6addr( & ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
+  uip_ds6_set_addr_iid( & ipaddr, & uip_lladdr);
+  uip_ds6_addr_add( & ipaddr, 0, ADDR_AUTOCONF);
 
   printf("IPv6 addresses: ");
-  for(i = 0; i < UIP_DS6_ADDR_NB; i++) {
+  for (i = 0; i < UIP_DS6_ADDR_NB; i++) {
     state = uip_ds6_if.addr_list[i].state;
-    if(uip_ds6_if.addr_list[i].isused &&
-       (state == ADDR_TENTATIVE || state == ADDR_PREFERRED)) {
-      uip_debug_ipaddr_print(&uip_ds6_if.addr_list[i].ipaddr);
+    if (uip_ds6_if.addr_list[i].isused &&
+      (state == ADDR_TENTATIVE || state == ADDR_PREFERRED)) {
+      uip_debug_ipaddr_print( & uip_ds6_if.addr_list[i].ipaddr);
       printf("\n");
     }
   }
 }
 
 /*---------------------------------------------------------------------------*/
+/**
+ * Processo que verifica o movimento. No caso da nossa simulação,
+ * esta verificação é feita através do botão do mote
+ */
 
-PROCESS_THREAD(button_process, ev, data)
-{
+PROCESS_THREAD(button_process, ev, data) {
   PROCESS_BEGIN();
-  static struct etimer et;
+  static struct etimer et; // timer to turn off the light
 
   SENSORS_ACTIVATE(button_sensor);
 
-  while(1) {
-	  PROCESS_WAIT_EVENT();
-    
-    if(ev == sensors_event &&
-		       data == &button_sensor){
-	    char led = leds_get();	
-		  if(led & LEDS_YELLOW){
-			  etimer_restart(&et);
-			  printf("Timer restart\n");
-		  }
-		  else{
-        etimer_set(&et, LIGHTS_TIME);
-			  leds_on(LEDS_YELLOW);
-			  printf("Ligths ON\n");
-        process_post_synch(&unicast_sender_process, PROCESS_EVENT_CONTINUE, "P 1");
-		  }
-	  }
-    else if(ev == PROCESS_EVENT_TIMER) {  // If the event it's provoked by the timer expiration, then...
-      leds_off(LEDS_YELLOW);	
-		  printf("Lights OFF\n");
-      process_post_synch(&unicast_sender_process, PROCESS_EVENT_CONTINUE, "P 0");
+  while (1) {
+    PROCESS_WAIT_EVENT();
+
+    if (ev == sensors_event &&
+      data == & button_sensor) {
+      char led = leds_get(); // check what LEDs are on	
+      if (led & LEDS_YELLOW) { // if the light is on, just restart the timer
+        etimer_restart( & et);
+        printf("Timer restart\n");
+      } else { // if the light is off, turn it on, start the timer and send a message to sink
+        etimer_set( & et, LIGHTS_TIME);
+        leds_on(LEDS_YELLOW);
+        printf("Ligths ON\n");
+        process_post_synch( & unicast_sender_process, PROCESS_EVENT_CONTINUE, "P 1");
+      }
+    } else if (ev == PROCESS_EVENT_TIMER) { // if the timer expires (no movemment), turn off the light and send that information to sink
+      leds_off(LEDS_YELLOW);
+      printf("Lights OFF\n");
+      process_post_synch( & unicast_sender_process, PROCESS_EVENT_CONTINUE, "P 0");
     }
   }
 
@@ -152,74 +151,83 @@ PROCESS_THREAD(button_process, ev, data)
 }
 
 /*---------------------------------------------------------------------------*/
+/** 
+ * Processo que lê os valores inseridos no serial monitor. 
+ * Este processo só existe na nossa simulação, caso estivesse
+ * disponível um sensor físico este processo era descartado;
+ */
 
-PROCESS_THREAD(temperature_process, ev, data)
-{
+PROCESS_THREAD(temperature_process, ev, data) {
   PROCESS_BEGIN();
 
-  while(1) {
-		PROCESS_WAIT_EVENT();
+  while (1) {
+    PROCESS_WAIT_EVENT();
 
-    if(ev == serial_line_event_message){
+    if (ev == serial_line_event_message) {
       temp_debug = atof(data);
-		}
+    }
   }
   PROCESS_END();
 }
 
 /*---------------------------------------------------------------------------*/
-
-PROCESS_THREAD(temperature_sampling_process, ev, data)
-{
+/**
+ * Processo que faz a amostragem dos valores do sensor 
+ * (neste caso, simulado pelo temperature_process). 
+ * Faz ainda uma pequena análise do valores (threshold)
+ */
+PROCESS_THREAD(temperature_sampling_process, ev, data) {
   PROCESS_BEGIN();
-  static struct etimer etemp;
-  etimer_set(&etemp, TEMPERATURE_TIME);
+  static struct etimer etemp; // interval to read sensor values
+  etimer_set( & etemp, TEMPERATURE_TIME); // start the timer
 
-  leds_on(LEDS_GREEN);
+  leds_on(LEDS_GREEN); // GREEN = temperature ok
 
-  while(1) {
-		PROCESS_WAIT_EVENT();
+  while (1) {
+    PROCESS_WAIT_EVENT();
 
-    if(ev == PROCESS_EVENT_TIMER){
-      etimer_reset(&etemp);
+    if (ev == PROCESS_EVENT_TIMER) {
+      etimer_reset( & etemp);
 
-      ///// read sensor
+      // read sensor if it was real
       // float temp = sensor.value;
       float temp = temp_debug;
-      temp_k++;
-      printf("Temperature: %.2fºC - max:%.1f\n",temp,(float)TEMPERATURE_MAX);
-      
-      temperature[temp_k-1] = temp;
-      if(temp_k==5){
-        // send values
-        char  msg[50];
+      temp_k++; // var to count how many values are saved
+      printf("Temperature: %.2f C - max:%.1f\n", temp, (float) TEMPERATURE_MAX);
+
+      temperature[temp_k - 1] = temp;
+      if (temp_k == 5) {
+        // send values to sink
+        char msg[50];
         sprintf(msg, "T %.1f %.1f %.1f %.1f %.1f", temperature[0], temperature[1], temperature[2], temperature[3], temperature[4]);
-        process_post_synch(&unicast_sender_process, PROCESS_EVENT_CONTINUE, msg);
-        
-        // default
-        temp_k=0;
+        process_post_synch( & unicast_sender_process, PROCESS_EVENT_CONTINUE, msg);
+
+        // reset counter
+        temp_k = 0;
       }
 
-			if(temp > TEMPERATURE_MAX){
-				leds_off(LEDS_GREEN);
-				leds_on(LEDS_RED);
-				printf("Temperature too high!\n");
-			}
-			else{
-				leds_off(LEDS_RED);
-				leds_on(LEDS_GREEN);
-				printf("Temperature OK\n");
-			}
-		}
+      if (temp > TEMPERATURE_MAX) {
+        leds_off(LEDS_GREEN); // GREEN = temperature ok
+        leds_on(LEDS_RED); // RED = temperature too high
+        printf("Temperature too high!\n");
+      } else {
+        leds_off(LEDS_RED);
+        leds_on(LEDS_GREEN);
+        printf("Temperature OK\n");
+      }
+    }
   }
   PROCESS_END();
 }
 
 /*---------------------------------------------------------------------------*/
+/**
+ * Processo responsável por estabelecer a ligação com o sink
+ * e enviar uma mensagem, este processo é chamado dentro de outros processos.
+ */
 
-PROCESS_THREAD(unicast_sender_process, ev, data)
-{
-  uip_ipaddr_t *addr;
+PROCESS_THREAD(unicast_sender_process, ev, data) {
+  uip_ipaddr_t * addr;
 
   PROCESS_BEGIN();
 
@@ -227,19 +235,19 @@ PROCESS_THREAD(unicast_sender_process, ev, data)
 
   set_global_address();
 
-  simple_udp_register(&unicast_connection, UDP_PORT,
-                      NULL, UDP_PORT, receiver);
+  simple_udp_register( & unicast_connection, UDP_PORT,
+    NULL, UDP_PORT, receiver);
 
-  while(1) {
+  while (1) {
     PROCESS_WAIT_EVENT();
-    if(ev == PROCESS_EVENT_CONTINUE){
+    if (ev == PROCESS_EVENT_CONTINUE) { // event called by the other processes
       addr = servreg_hack_lookup(SERVICE_ID);
-      if(addr != NULL) {
+      if (addr != NULL) {
 
         printf("Sending unicast to ");
         uip_debug_ipaddr_print(addr);
         printf("\n");
-        simple_udp_sendto(&unicast_connection, data, strlen(data) + 1, addr);
+        simple_udp_sendto( & unicast_connection, data, strlen(data) + 1, addr);
 
       } else {
         printf("Service %d not found\n", SERVICE_ID);
--- project2/final/mote_sink/unicast-receiver.c
+++ project2/final/mote_sink/unicast-receiver.c
@@ -37,11 +37,10 @@
 #include "net/ip/uip.h"
 #include "net/ipv6/uip-ds6.h"
 #include "net/ip/uip-debug.h"
+#include "net/rpl/rpl.h"
 
 #include "simple-udp.h"
 #include "servreg-hack.h"
-
-#include "net/rpl/rpl.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -51,133 +50,129 @@
 #define SERVICE_ID 190
 
 static struct simple_udp_connection unicast_connection;
-typedef struct Vector {
-  uip_ipaddr_t addr;
-  int presence;
-  float temperature[5];
-}vector;
-static vector *pvector;
-static int pvector_size;
+
+typedef struct Vector { // struct to save the information from the motes
+  uip_ipaddr_t addr; // address of the sender mote
+  int presence; // 0 = no movemment, 1 = movemment
+  float temperature[5]; // last 5 values
+}
+vector;
+static vector * pvector; // dynamically allocated array
+static int pvector_size; // size of the array
 
 /*---------------------------------------------------------------------------*/
 PROCESS(unicast_receiver_process, "Unicast receiver example process");
 PROCESS(button_process, "Button process");
-AUTOSTART_PROCESSES(&unicast_receiver_process,&button_process);
+AUTOSTART_PROCESSES( & unicast_receiver_process, & button_process);
 /*---------------------------------------------------------------------------*/
 static void
-fill_values(const uip_ipaddr_t *sender_addr,
-            const uint8_t *data)
-{
-  if(pvector==NULL){
-    pvector = (vector *)malloc(sizeof(vector));
+fill_values(const uip_ipaddr_t * sender_addr,
+  const uint8_t * data) {
+  if (pvector == NULL) { // check if there is no data saved
+    pvector = (vector * ) malloc(sizeof(vector)); // allocate memory of one vector struct
     pvector_size = 1;
-    uip_ip6addr(&pvector[0].addr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
-    uip_ipaddr_copy(&pvector[0].addr, sender_addr);
-    //printf("sender_addr: ");
-    //uip_debug_ipaddr_print(&pvector[0].addr);
-    //printf("\n");
-    if(data[0]=='P'){
-      sscanf((char*)data, "P %d", &pvector[0].presence);
-      //printf("P = %d\n",pvector[0].presence);
-    }
-    else if(data[0]=='T'){
+    uip_ip6addr( & pvector[0].addr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
+    uip_ipaddr_copy( & pvector[0].addr, sender_addr);
+    if (data[0] == 'P') { // check if the incoming info is about presence or temperature
+      sscanf((char * ) data, "P %d", & pvector[0].presence);
+      int i;
+      for (i = 0; i < 5; i++) pvector[0].temperature[i] = 0;
+    } else if (data[0] == 'T') {
       pvector[0].presence = 0;
-      sscanf((char*)data, "T %f %f %f %f %f", &pvector[0].temperature[0], &pvector[0].temperature[1], &pvector[0].temperature[2], &pvector[0].temperature[3], &pvector[0].temperature[4]);
-      //printf("P = %d\n",pvector[0].presence);
-      //printf("T = %f %f %f %f %f\n", pvector[0].temperature[0], pvector[0].temperature[1], pvector[0].temperature[2], pvector[0].temperature[3], pvector[0].temperature[4]);
+      sscanf((char * ) data, "T %f %f %f %f %f", & pvector[0].temperature[0], & pvector[0].temperature[1], & pvector[0].temperature[2], & pvector[0].temperature[3], & pvector[0].temperature[4]);
     }
-  }
-  else{
+  } else { // if there's already data saved
     int exist = 0;
     int i;
-    for(i = 0; i<pvector_size; i++){
-      if(uip_ipaddr_cmp(&pvector[i].addr, sender_addr)){
+    for (i = 0; i < pvector_size; i++) { // exist = 1, if there is data related to this mote
+      if (uip_ipaddr_cmp( & pvector[i].addr, sender_addr)) { // compare the address
         exist = 1;
         break;
       }
     }
-    if(!exist){
-      pvector = (vector *)realloc(pvector, (++pvector_size)*sizeof(vector));
-      uip_ip6addr(&pvector[i].addr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
-      uip_ipaddr_copy(&pvector[i].addr, sender_addr);
+    if (!exist) { // if not exists, create
+      pvector = (vector * ) realloc(pvector, (++pvector_size) * sizeof(vector));
+      uip_ip6addr( & pvector[i].addr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
+      uip_ipaddr_copy( & pvector[i].addr, sender_addr);
       pvector[i].presence = 0;
+      int k;
+      for (k = 0; k < 5; k++) pvector[i].temperature[k] = 0;
     }
 
-    if(data[0]=='P'){
-      sscanf((char*)data, "P %d", &pvector[i].presence);
-      //printf("P = %d\n",pvector[i].presence);
-    }
-    else if(data[0]=='T'){
-      sscanf((char*)data, "T %f %f %f %f %f", &pvector[i].temperature[0], &pvector[i].temperature[1], &pvector[i].temperature[2], &pvector[i].temperature[3], &pvector[i].temperature[4]);
-      //printf("P = %d\n",pvector[i].presence);
-      //printf("T = %f %f %f %f %f\n", pvector[i].temperature[0], pvector[i].temperature[1], pvector[i].temperature[2], pvector[i].temperature[3], pvector[i].temperature[4]);
+    // save the information
+    if (data[0] == 'P') {
+      sscanf((char * ) data, "P %d", & pvector[i].presence);
+    } else if (data[0] == 'T') {
+      sscanf((char * ) data, "T %f %f %f %f %f", & pvector[i].temperature[0], & pvector[i].temperature[1], & pvector[i].temperature[2], & pvector[i].temperature[3], & pvector[i].temperature[4]);
     }
   }
 }
 /*---------------------------------------------------------------------------*/
 static void
-receiver(struct simple_udp_connection *c,
-         const uip_ipaddr_t *sender_addr,
-         uint16_t sender_port,
-         const uip_ipaddr_t *receiver_addr,
-         uint16_t receiver_port,
-         const uint8_t *data,
-         uint16_t datalen)
-{
+receiver(struct simple_udp_connection * c,
+        const uip_ipaddr_t * sender_addr,
+        uint16_t sender_port,
+        const uip_ipaddr_t * receiver_addr,
+        uint16_t receiver_port,
+        const uint8_t * data,
+        uint16_t datalen) {
   printf("Data received from ");
   uip_debug_ipaddr_print(sender_addr);
   printf(" on port %d from port %d with length %d: '%s'\n",
-         receiver_port, sender_port, datalen, data);
+    receiver_port, sender_port, datalen, data);
   fill_values(sender_addr, data);
 }
 /*---------------------------------------------------------------------------*/
 static uip_ipaddr_t *
-set_global_address(void)
-{
-  static uip_ipaddr_t ipaddr;
-  int i;
-  uint8_t state;
+  set_global_address(void) {
+    static uip_ipaddr_t ipaddr;
+    int i;
+    uint8_t state;
 
-  uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
-  uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
-  uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
+    uip_ip6addr( & ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
+    uip_ds6_set_addr_iid( & ipaddr, & uip_lladdr);
+    uip_ds6_addr_add( & ipaddr, 0, ADDR_AUTOCONF);
 
-  printf("IPv6 addresses: ");
-  for(i = 0; i < UIP_DS6_ADDR_NB; i++) {
-    state = uip_ds6_if.addr_list[i].state;
-    if(uip_ds6_if.addr_list[i].isused &&
-       (state == ADDR_TENTATIVE || state == ADDR_PREFERRED)) {
-      uip_debug_ipaddr_print(&uip_ds6_if.addr_list[i].ipaddr);
-      printf("\n");
+    printf("IPv6 addresses: ");
+    for (i = 0; i < UIP_DS6_ADDR_NB; i++) {
+      state = uip_ds6_if.addr_list[i].state;
+      if (uip_ds6_if.addr_list[i].isused &&
+        (state == ADDR_TENTATIVE || state == ADDR_PREFERRED)) {
+        uip_debug_ipaddr_print( & uip_ds6_if.addr_list[i].ipaddr);
+        printf("\n");
+      }
     }
-  }
 
-  return &ipaddr;
-}
+    return &ipaddr;
+  }
 /*---------------------------------------------------------------------------*/
 static void
-create_rpl_dag(uip_ipaddr_t *ipaddr)
-{
-  struct uip_ds6_addr *root_if;
+create_rpl_dag(uip_ipaddr_t * ipaddr) {
+  struct uip_ds6_addr * root_if;
 
   root_if = uip_ds6_addr_lookup(ipaddr);
-  if(root_if != NULL) {
-    rpl_dag_t *dag;
+  if (root_if != NULL) {
+    rpl_dag_t * dag;
     uip_ipaddr_t prefix;
-    
+
     rpl_set_root(RPL_DEFAULT_INSTANCE, ipaddr);
     dag = rpl_get_any_dag();
-    uip_ip6addr(&prefix, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
-    rpl_set_prefix(dag, &prefix, 64);
+    uip_ip6addr( & prefix, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
+    rpl_set_prefix(dag, & prefix, 64);
     PRINTF("created a new RPL dag\n");
   } else {
     PRINTF("failed to create a new RPL DAG\n");
   }
 }
+
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(unicast_receiver_process, ev, data)
-{
-  uip_ipaddr_t *ipaddr;
+/**
+ * Processo responsável por estabelecer a ligação com os motes e 
+ * receber/guardar informação.
+ */
+
+PROCESS_THREAD(unicast_receiver_process, ev, data) {
+  uip_ipaddr_t * ipaddr;
 
   PROCESS_BEGIN();
 
@@ -189,37 +184,41 @@ PROCESS_THREAD(unicast_receiver_process, ev, data)
 
   servreg_hack_register(SERVICE_ID, ipaddr);
 
-  simple_udp_register(&unicast_connection, UDP_PORT,
-                      NULL, UDP_PORT, receiver);
+  simple_udp_register( & unicast_connection, UDP_PORT,
+    NULL, UDP_PORT, receiver);
 
-  while(1) {
+  while (1) {
     PROCESS_WAIT_EVENT();
   }
   free(pvector);
   PROCESS_END();
 }
-/*---------------------------------------------------------------------------*/
 
-PROCESS_THREAD(button_process, ev, data)
-{
+/*---------------------------------------------------------------------------*/
+/**
+ * Processo que imprime as informações guardadas para visualização
+ * do usuário quando o botão é acionado
+ */
+
+PROCESS_THREAD(button_process, ev, data) {
   PROCESS_BEGIN();
 
   SENSORS_ACTIVATE(button_sensor);
 
-  while(1) {
-	  PROCESS_WAIT_EVENT();
-    
-    if(ev == sensors_event &&
-		       data == &button_sensor){
-	    printf("\n\nDATA:\n");
+  while (1) {
+    PROCESS_WAIT_EVENT();
+
+    if (ev == sensors_event &&
+      data == & button_sensor) {
+      printf("\n\nDATA:\n");
       int i;
-      for(i = 0; i<pvector_size; i++){
+      for (i = 0; i < pvector_size; i++) {
         printf("ADDR: ");
-        uip_debug_ipaddr_print(&pvector[i].addr);
-        printf("\nP = %d\n",pvector[i].presence);
+        uip_debug_ipaddr_print( & pvector[i].addr);
+        printf("\nP = %d\n", pvector[i].presence);
         printf("T = %.1f %.1f %.1f %.1f %.1f\n\n", pvector[i].temperature[0], pvector[i].temperature[1], pvector[i].temperature[2], pvector[i].temperature[3], pvector[i].temperature[4]);
       }
-	  }
+    }
   }
 
   PROCESS_END();
